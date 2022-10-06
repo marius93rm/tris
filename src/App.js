676157a5fc96153a645c33d8bd5ada08e575d75f
marius93rm <marius93rm@gmail.com>
2022-10-06 12:01:26 +0200

    set frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React, { useState } from 'react';
+
+function Square(props) {
+  return (
+    <button className="square">
+      {props.value}
+    </button>
+  );
+
+}
+
+function Board() {
+  const [board, setBoard] = useState(Array(9).fill(0))
+  const [xIsNext, setXIsNext] = useState(true)
+
+  const giocatoreInCorso = xIsNext ? 'X' : 'O'
+
+  function renderSquare(i) {
+    return <Square value={board[i]} />;
+  }
+
+  return (
+    <div>
+      <div className="status">{"è il turno di " + giocatoreInCorso}</div>
+
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+
+
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
     </div>
   );
 }
